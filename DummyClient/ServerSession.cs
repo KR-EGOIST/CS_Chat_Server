45a4ca7589d0d394740a4006d02c78b25c87abf7
Yun Jinho <jinho504136@gmail.com>
2024-10-17 23:27:09 +0900

    ♻️ refactor : 패킷 직렬화/역직렬화 세션 분리

---
+++ DummyClient/ServerSession.cs
@@ -0,0 +1,87 @@
+﻿using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyClient
+{
+    class Packet
+    {
+        public ushort size;
+        public ushort packetId;
+    }
+
+    class PlayerInfoReq : Packet
+    {
+        public long playerId;
+    }
+
+    class PlayerInfoOk : Packet
+    {
+        public int hp;
+        public int attack;
+    }
+
+    public enum PacketID
+    {
+        PlayerInfoReq = 1,
+        PlayerInfoOk = 2,
+    }
+
+    class ServerSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected: {endPoint}");
+
+            PlayerInfoReq packet = new PlayerInfoReq() { packetId = (ushort)PacketID.PlayerInfoReq, playerId = 1001 };
+
+            try
+            {
+                //for (int i = 0; i < 5; ++i)
+                {
+                    ArraySegment<byte> openSegment = SendBufferHelper.Open(4096);
+                    
+                    ushort count = 0;
+                    bool success = true;
+
+                    count += 2;
+                    success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset + count, openSegment.Count - count), packet.packetId);
+                    count += 2;
+                    success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset + count, openSegment.Count - count), packet.playerId);
+                    count += 8;
+                    success &= BitConverter.TryWriteBytes(new Span<byte>(openSegment.Array, openSegment.Offset, openSegment.Count), count);
+
+                    ArraySegment<byte> sendBuff = SendBufferHelper.Close(count);
+
+                    if(success)
+                        Send(sendBuff);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected: {endPoint}");
+        }
+
+        public override int OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Server] {recvData}");
+            return buffer.Count;
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
+}
