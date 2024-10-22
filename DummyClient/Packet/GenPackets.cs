cd79eef3356168353e7f0057151ce8b4a52da401
Yun Jinho <jinho504136@gmail.com>
2024-10-23 01:16:06 +0900

    ✨ update : 공용으로 사용가능하도록 Interface 패킷 생성 및 상속

--- DummyClient/Packet/GenPackets.cs
+++ DummyClient/Packet/GenPackets.cs
@@ -11,8 +11,15 @@ public enum PacketID
 	
 }
 
+interface IPacket
+{
+	ushort Protocol { get; }
+	void Read(ArraySegment<byte> segment);
+	ArraySegment<byte> Write();
+}
 
-class PlayerInfoReq
+
+class PlayerInfoReq : IPacket
 {
     public byte testByte;
 	public long playerId;
@@ -80,6 +87,8 @@ class PlayerInfoReq
 	}
 	public List<Skill> skills = new List<Skill>();
 
+    public ushort Protocol { get { return (ushort)PacketID.PlayerInfoReq; } }
+
     public void Read(ArraySegment<byte> segment)
     {
         ushort count = 0;
@@ -137,9 +146,11 @@ class PlayerInfoReq
     }
 }
 
-class Test
+class Test : IPacket
 {
     public int testInt;
+
+    public ushort Protocol { get { return (ushort)PacketID.Test; } }
 
     public void Read(ArraySegment<byte> segment)
     {
