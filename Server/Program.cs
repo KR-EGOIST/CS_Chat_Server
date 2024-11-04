1ce6f04a13a519539e7624d9bd3c153c4e4990b7
Yun Jinho <jinho504136@gmail.com>
2024-11-04 16:34:10 +0900

    ✨ update : JobTimer 구현

--- Server/Program.cs
+++ Server/Program.cs
@@ -10,6 +10,12 @@ namespace Server
         static Listener _listener = new Listener();
         public static GameRoom Room = new GameRoom();
 
+        static void FlushRoom()
+        {
+            Room.Push(() => Room.Flush());
+            JobTimer.Instance.Push(FlushRoom, 250); // 250ms 후에 다시 실행시켜줘
+        }
+
         static void Main(string[] args)
         {
             // DNS (Domain Name System)
@@ -21,10 +27,12 @@ namespace Server
             _listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
             Console.WriteLine("Listening...");
 
+            //FlushRoom();
+            JobTimer.Instance.Push(FlushRoom);
+
             while (true)
             {
-                Room.Push(() => Room.Flush());
-                Thread.Sleep(250);
+                JobTimer.Instance.Flush();
             }
         }
     }
