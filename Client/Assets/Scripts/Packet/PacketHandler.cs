6ecd9069aa342de01d12818eeba8b4f73143b810
Yun Jinho <jinho504136@gmail.com>
2024-11-04 22:52:29 +0900

    🎉 add : unity 연동 테스트

---
+++ Client/Assets/Scripts/Packet/PacketHandler.cs
@@ -0,0 +1,23 @@
+﻿using DummyClient;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+class PacketHandler
+{
+    // 어떤 세션에서 되었는지, 어떤 패킷을 받아온건지 인자로 받는다.
+    public static void S_ChatHandler(PacketSession session, IPacket packet)
+    {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession serverSession = session as ServerSession;
+
+        if (chatPacket.playerId == 1)
+            Debug.Log(chatPacket.chat);
+        //if(chatPacket.playerId == 1)
+            //Console.WriteLine(chatPacket.chat);
+    }
+}
